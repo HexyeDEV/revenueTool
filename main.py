fb2b8096f38526817bfb675a92ea308ba55e7ac6
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-13 15:32:12 +0200

    Added Add Bill button

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import tkinter as tk
 from tkinter import messagebox, Toplevel, ttk, PhotoImage
 from ttkbootstrap import Style
-import time
-
-progress = {}
 
 def move(event):
     window.gemoetry(f'+{0}+{1}'.format(event.x_root, event.y_root))
 
-def close():
-    global window
-    window.destroy()
+
+def addbillpressed():
+    global window, screen, reason, amount
+    if reason.get() != '' and amount.get() != '':
+        messagebox.showinfo('DONE', 'Bill Succesfully Added!')
+        screen.destroy()
+    else:
+        messagebox.showerror('ERROR', 'Please fill all details')
+
+def addbill():
+    global window, screen, reason, amount
+    screen = Toplevel(window)
+    screen.title('Add a Bill')
+    screen.geometry('500x500')
+    style = Style(screen)
+    style.theme_use('cyborg')
+    ttk.Label(screen, text='Payed For:').pack()
+    reason = ttk.Entry(screen)
+    reason.pack()
+    ttk.Label(screen, text='').pack()
+    ttk.Label(screen, text='Amount Payed:').pack()
+    amount = ttk.Entry(screen)
+    amount.pack()
+    ttk.Label(screen, text='').pack()
+    ttk.Button(screen, text='Add Bill', command=addbillpressed).pack()
 
 def main():
     global window, splashscreen
@@ -20,13 +39,14 @@ def main():
     style = Style(window)
     style.theme_use('cyborg')
     window.title("REVENUE TOOLS")
+    ttk.Button(window, text='Add a Bill', command=addbill).pack()
     window.mainloop()
 
 global splashscreen
 splashscreen = tk.Tk()
 splashscreen.geometry('300x250')
 splashscreen.overrideredirect(1)
-tk.Label(splashscreen, text='REVENUE MANAGER').pack()
+tk.Label(splashscreen, text='REVENUE TOOLS').pack()
 tk.Label(splashscreen, text='').pack()
 per = tk.Label(splashscreen, text='Loading...')
 per.pack()
